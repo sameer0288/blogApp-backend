dd881a116e2c0403d7094e6b3482c8530d3287a8
sameer0288 <153083473+sameer-jethitech@users.noreply.github.com>
2023-12-23 11:12:25 +0530

    backend code

---
+++ server.js
@@ -0,0 +1,175 @@
+// Import required modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Add this line
+
+// Initialize Express app
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware
+app.use(helmet());
+app.use(express.json());
+app.use(cors()); // Add this line
+
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use("/api/", limiter);
+
+// MongoDB connection string
+const mongoURI =
+  "mongodb+srv://sameersheikh0288:sameer1234@cluster0.y7meqjk.mongodb.net/";
+
+// Set up MongoDB connection
+mongoose.connect(mongoURI);
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Event listeners for MongoDB connection events
+
+// When successfully connected
+db.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+// When there is an error in the connection
+db.on("error", (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+// When the connection is disconnected
+db.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
+
+// Close the MongoDB connection when the Node.js process terminates
+process.on("SIGINT", () => {
+  db.close(() => {
+    console.log("MongoDB connection closed through app termination");
+    process.exit(0);
+  });
+});
+
+// Rest of the code remains unchanged...
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+// Blog post schema
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+// Models
+const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", postSchema);
+
+// Express routes
+app.post("/api/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    const token = jwt.sign({ userId: user._id }, "secretKey", {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Middleware to verify JWT
+const authenticateUser = (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "secretKey");
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Authentication failed" });
+  }
+};
+
+app.use("/api/posts", authenticateUser);
+
+// Blog post CRUD operations
+app.get("/api/posts", async (req, res) => {
+  try {
+    const posts = await Post.find().populate("author", "username");
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/posts", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const newPost = new Post({ title, content, author: req.userId });
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/api/posts/:postId", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      { title, content },
+      { new: true }
+    );
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/api/posts/:postId", async (req, res) => {
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
